0ed3c6310ee88844fe0d96e1d88c329b12c4ee44
trideptraino1 <108325614+trideptraino1@users.noreply.github.com>
2022-07-11 10:43:20 +0700

    bai2

---
+++ tuan5/delfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+i=1;
+for argument in "$@" 
+do
+    head -1 $argument
+    i=$((i + 1));
+done	
+
+echo "ban co muon xoa cac file tren: y/n?"
+read choice
+case $choice in
+	y)
+		i=1;
+		for argument in "$@" 
+		do
+		    rm $argument
+		    i=$((i + 1));
+		done
+		echo "cac file tren da xoa"
+	;;
+	n)
+		echo"ban da khong xoa cac file"
+	;;
+esac
